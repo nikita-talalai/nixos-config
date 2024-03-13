9230427054a7b0ff256187c54fa60465e0387845
Nikita Talalai <nikita.talalai.2002@gmail.com>
2024-03-13 03:06:19 +0300

    feat(lightdm): add new display manager

--- nixos/display-manager.nix
+++ nixos/display-manager.nix
@@ -1,17 +1,27 @@
 { pkgs, ... }:
 
 {
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
-        user = "greeter";
+   services.xserver.enable = true;
+   services.xserver = {
+      displayManager.lightdm = {
+         enable = true;
       };
-    };
-  };
+      windowManager.hypr = {
+         enable = true;
+      };
+   };
 
-  environment.systemPackages = with pkgs; [
-    greetd.tuigreet
-  ];
+  # services.greetd = {
+  #   enable = true;
+  #   settings = {
+  #     default_session = {
+  #       command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
+  #       user = "greeter";
+  #     };
+  #   };
+  # };
+  #
+  # environment.systemPackages = with pkgs; [
+  #   greetd.tuigreet
+  # ];
 }
