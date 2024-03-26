5329af4ba6e0eac13a87ce7dbb4c38bbfaf55908
Nikita Talalai <nikita.talalai.2002@gmail.com>
2024-03-26 20:25:43 +0300

    feat(xmonad): add xmonad contrib and extra

--- home-manager/programs/xmonad/xmonad.hs
+++ home-manager/programs/xmonad/xmonad.hs
@@ -17,7 +17,6 @@
 import XMonad
 import Data.Monoid
 import System.Exit
--- import XMonad.Layout.StackTile
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
@@ -213,14 +212,12 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = tiled ||| Mirror tiled ||| Full -- ||| stack
+myLayout = tiled ||| Mirror tiled ||| Full
   where
     tiled   = Tall nmaster delta ratio
     nmaster = 2
     ratio   = 1/2
     delta   = 3/100
-
-    -- stack = St ackTile 1 (3/100) (1/2)
 ------------------------------------------------------------------------
 -- Window rules:
 
@@ -291,7 +288,7 @@ myStartupHook = return ()
 
 -- Run xmonad with the settings you specify. No need to modify this.
 --
-main = xmonad defaults
+main = xmonad . withMetrics defaults
 
 -- A structure containing your configuration settings, overriding
 -- fields in the default config. Any you don't override, will
