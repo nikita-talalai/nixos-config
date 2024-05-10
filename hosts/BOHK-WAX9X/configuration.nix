7f3a346887acfaa4d3cd2bd847218e612f2beef3
Nikita Talalai <nikita.talalai.2002@gmail.com>
2024-05-10 16:29:57 +0300

    feat: add configuration for laptop

---
+++ hosts/BOHK-WAX9X/configuration.nix
@@ -0,0 +1,99 @@
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [./hardware-configuration.nix];
+
+  nixpkgs = {config = {allowUnfree = true;};};
+
+  nix.registry =
+    (lib.mapAttrs (_: flake: {inherit flake;}))
+    ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+
+  nix.nixPath = ["/etc/nix/path"];
+  environment.etc =
+    lib.mapAttrs' (name: value: {
+      name = "nix/path/${name}";
+      value.source = value.flake;
+    })
+    config.nix.registry;
+
+  nix.settings = {
+    experimental-features = "nix-command flakes";
+    auto-optimise-store = true;
+  };
+
+  networking = {
+	hostName = "BOHK-WAX9X";
+	networkmanager.enable = true;
+  };
+
+  boot.loader.systemd-boot.enable = true;
+
+  users.users = {
+    nikita = {
+      isNormalUser = true;
+      extraGroups = ["wheel" "input" "uinput"];
+      shell = pkgs.zsh;
+    };
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+    };
+  };
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  security.polkit.enable = true;
+
+  hardware = {
+    opengl.enable = true;
+    nvidia.modesetting.enable = true;
+  };
+
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  programs = {
+    zsh.enable = true;
+    hyprland = {
+      enable = true;
+      xwayland.enable = true;
+    };
+  };
+
+  time.timeZone = "Europe/Moscow";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ru_RU.UTF-8";
+    LC_IDENTIFICATION = "ru_RU.UTF-8";
+    LC_MEASUREMENT = "ru_RU.UTF-8";
+    LC_MONETARY = "ru_RU.UTF-8";
+    LC_NAME = "ru_RU.UTF-8";
+    LC_NUMERIC = "ru_RU.UTF-8";
+    LC_PAPER = "ru_RU.UTF-8";
+    LC_TELEPHONE = "ru_RU.UTF-8";
+    LC_TIME = "ru_RU.UTF-8";
+  };
+
+  system.stateVersion = "23.11";
+}
--- hosts/default.nix
+++ hosts/default.nix
@@ -4,5 +4,9 @@
       specialArgs = {inherit inputs;};
       modules = [./z270a];
     };
+    BOHK-WAX9X = inputs.nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [./BOHK-WAX9X];
+    };
   };
 }
